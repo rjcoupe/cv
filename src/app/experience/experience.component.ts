c7383bc71e39806db27bb9a3e441d9cf1a8643b2
Richard Coupe <129647+rjcoupe@users.noreply.github.com>
2022-06-24 10:34:15 +0100

    Corrected title at Seccl

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -47,7 +47,7 @@ export class ExperienceComponent implements OnInit {
       ]
     },
     {
-      title: 'DevOps Lead',
+      title: 'Engineering Manager - DevOps',
       employer: 'Seccl',
       location: 'Remote/Bath',
       dateFrom: 'December 2020',
